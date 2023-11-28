0ee030e4b35964bbcad16819a2c17a454b6547c8
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-28 19:55:48 +0530

    Extended the genericMax

--- src/App.java
+++ src/App.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("TEST MAXIMUM :\n");
-        System.out.println("Enter the 3 Strings for checking:");
+        // System.out.println("Enter the 3 Strings for checking:");
         Scanner sc = new Scanner(System.in);
 
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
-        String str3 = sc.nextLine();
+        // String str1 = sc.nextLine();
+        // String str2 = sc.nextLine();
+        // String str3 = sc.nextLine();
 
-        GenericMax<String> maxString = new GenericMax<>(str1, str2, str3);
-        maxString.testMax();
+        // GenericMax<String> maxString = new GenericMax<>(str1, str2, str3);
+        // maxString.testMax();
 
-        System.out.println("\nEnter the 3 numbers for checking:");
-        Integer num1 = sc.nextInt();
-        Integer num2 = sc.nextInt();
-        Integer num3 = sc.nextInt();
-        GenericMax<Integer> maxNum = new GenericMax<>(num1, num2, num3);
-        maxNum.testMax();      
+        // System.out.println("\nEnter the 3 numbers for checking:");
+        // Integer num1 = sc.nextInt();
+        // Integer num2 = sc.nextInt();
+        // Integer num3 = sc.nextInt();
+        // GenericMax<Integer> maxNum = new GenericMax<>(num1, num2, num3);
+        // maxNum.testMax();  
+        
+        System.out.println("Enter the numver of elements :");
+        int n = sc.nextInt();
+
+        Integer[] intArr = new Integer[n];
+
+        for(int i =0;i<n;i++){
+            System.out.println("Enter element "+ (i+1));
+            intArr[i]=sc.nextInt();
+        }
+
+        GenericMax<Integer> maxFind = new GenericMax<>(intArr);
+        maxFind.testMax();
 
         sc.close();
     }
--- src/GenericMax.java
+++ src/GenericMax.java
@@ -1,29 +1,28 @@
 public class GenericMax<T extends Comparable<T>>{
-    private T var1;
-    private T var2;
-    private T var3;
+    
+    private T[] values;
 
-    public GenericMax(T var1, T var2, T var3){
-        this.var1 = var1;
-        this.var2 = var2;
-        this.var3 = var3;
+    public GenericMax(T... values){
+        this.values = values;
+        
     }
-    public static <T extends Comparable<T>> T Compare(T obj1, T obj2, T obj3) {
-        int res1 = obj1.compareTo(obj2);
-        int res2 = obj2.compareTo(obj3);
-        int res3 = obj3.compareTo(obj1);
-
-        if (res1 > 0 && res2 > 0) {
-            return obj1;
-        } else if (res2 > 0 && res3 > 0) {
-            return obj2;
-        } else {
-            return obj3;
+    public static <T extends Comparable<T>> T Compare(T... values) {
+        
+        if(values == null || values.length == 0){
+            return null;
         }
+
+        T maxi = values[0];
+        for(int i=1;i<values.length; i++){
+            if(values[i].compareTo(maxi)>0){
+                maxi = values[i];
+            }
+        }
+        return maxi;
     }
 
     public void testMax(){
-        T maxi = Compare(var1, var2, var3);
+        T maxi = Compare(values);
         System.out.println("THE MAXIMUM VALUE :"+ maxi);
     }
 
